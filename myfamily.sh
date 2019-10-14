60cff96add579a0955fdc3169fd0d412df9f35b1
Qjawko <Qjawko@gmail.com>
2019-10-15 00:01:35 +0600

    fast fix

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,1 @@
-curl 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq -c -j ".[] | select( .id == $HERO_ID ) | .connections.relatives"
-
+curl  https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg ID_num "$HERO_ID" '.[] | select( .id ==($ID_num|tonumber)) | .connections.relatives' | sed 's/"//g'
