56e90c2a84cfa46e8b9b73e4e57fe74d7a270ac3
Qjawko <Qjawko@gmail.com>
2019-10-16 23:50:21 +0600

    printcom2

--- printcomb2.go
+++ printcomb2.go
@@ -1,36 +1,36 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := 0; i <= 98; i++ {
-		for j := 0; i < 99; j++ {
-			if i < j {
-				if i == 98 && j == 99 {
-					z01.PrintRune((rune)(i / 10))
-					z01.PrintRune((rune)(i % 10))
-
-					z01.PrintRune(' ')
-
-					z01.PrintRune((rune)(j / 10))
-					z01.PrintRune((rune)(j % 10))
-
-					z01.PrintRune(10)
-				} else {
-					z01.PrintRune((rune)(i / 10))
-					z01.PrintRune((rune)(i % 10))
-
-					z01.PrintRune(' ')
-
-					z01.PrintRune((rune)(j / 10))
-					z01.PrintRune((rune)(j % 10))
-
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
+					if i < k {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+						if i != '9' && j != '8' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					} else if i == k && j < l {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+						if i != '9' && j != '8' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
 				}
 			}
 		}
 	}
+	z01.PrintRune(10)
 }
