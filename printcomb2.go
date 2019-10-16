9f9a39e65e90321a567cb7c73ce9e31160f6a24b
Qjawko <Qjawko@gmail.com>
2019-10-16 23:52:38 +0600

    printcom2

--- printcomb2.go
+++ printcomb2.go
@@ -13,7 +13,7 @@ func PrintComb2() {
 						z01.PrintRune(' ')
 						z01.PrintRune(k)
 						z01.PrintRune(l)
-						if i != '9' && j != '8' {
+						if i != '9' || j != '8' {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}
@@ -23,7 +23,7 @@ func PrintComb2() {
 						z01.PrintRune(' ')
 						z01.PrintRune(k)
 						z01.PrintRune(l)
-						if i != '9' && j != '8' {
+						if i != '9' || j != '8' {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}
