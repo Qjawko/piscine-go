d483deadebd28f9b788044b7f040d27c292ee2c1
Qjawko <Qjawko@gmail.com>
2019-10-16 19:57:05 +0600

    isnegative submit

---
+++ piscine/piscine.go
@@ -0,0 +1,14 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+//IsNegative prints is negative number or not
+func IsNegative(nb int) {
+	if nb > 0 {
+		z01.PrintRune('F')
+	} else {
+		z01.PrintRune('T')
+	}
+}
