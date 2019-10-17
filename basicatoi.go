13b4458fe78e20ae83a96057c4cd0a22b87a7fff
Qjawko <Qjawko@gmail.com>
2019-10-17 19:51:15 +0600

    basicatoi fix

--- basicatoi.go
+++ basicatoi.go
@@ -2,10 +2,18 @@ package piscine
 
 func BasicAtoi(s string) int {
 	nm := 0
-	for _, ch := range []byte(s) {
-		ch -= '0'
-		nm = nm*10 + int(ch)
+	for _, ch := range s {
+		nm = nm*10 + charToInt(ch)
 	}
 
 	return nm
+}
+
+func charToInt(ch rune) int {
+	count := 0
+	for i := '1'; i <= ch; i++ {
+		count++
+	}
+
+	return count
 }
