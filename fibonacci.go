d7784481d337d258ea4e8ba6949e309e7f740406
Qjawko <Qjawko@DESKTOP-IDL9KO0>
2019-10-21 16:53:16 +0600

    format fix

--- fibonacci.go
+++ fibonacci.go
@@ -7,9 +7,9 @@ func Fibonacci(index int) int {
 
 	if index == 0 {
 		return 0
-	} else if index == 1{
+	} else if index == 1 {
 		return 1
 	}
 
-	return Fibonacci(index - 1) + Fibonacci(index - 2)
+	return Fibonacci(index-1) + Fibonacci(index-2)
 }
