db13a7d8c7feaceb7c4d7a5033170d57bb43274f
Qjawko <Qjawko@gmail.com>
2019-10-22 19:21:03 +0600

    added if

--- nrune.go
+++ nrune.go
@@ -1,5 +1,18 @@
 package piscine
 
 func NRune(s string, n int) rune {
+	if n > RuneLen([]rune(s)) {
+		return '0'
+	}
+
 	return []rune(s)[n-1]
+}
+
+func RuneLen(runes []rune) int {
+	count := 0
+	for range runes {
+		count++
+	}
+
+	return count
 }
