bcf2564cbb9d9403275454b348f6b30620e6d2fb
Qjawko <Qjawko@gmail.com>
2019-10-22 19:55:11 +0600

    index.go

---
+++ index.go
@@ -0,0 +1,43 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	runes := []rune(s)
+	runesToFind := []rune(toFind)
+	n := RuneLen(runesToFind)
+
+	switch {
+	case n == 0:
+		return 0
+	case n == 1:
+		return IndexRune(s, []rune(s)[n]) + 1
+	}
+
+	for i := range runes {
+		if runesToFind[0] == runes[i] {
+			flag := false
+			for j := range runesToFind {
+				if runes[i+j] == runesToFind[j] {
+					flag = true
+				} else {
+					flag = false
+				}
+			}
+
+			if flag {
+				return i
+			}
+		}
+	}
+
+	return -1
+}
+
+func IndexRune(s string, r rune) int {
+	for i, ch := range []rune(s) {
+		if ch == r {
+			return i
+		}
+	}
+
+	return -1
+}
