3a7e1f4b7e472a152f2e9b7f148448756cd8be68
Qjawko <Qjawko@gmail.com>
2019-10-22 23:27:28 +0600

    join.go

--- join.go
+++ join.go
@@ -2,8 +2,17 @@ package piscine
 
 func Join(strs []string, sep string) string {
 	var result string
-	for _, str := range strs {
-		result += str + sep
+	for i := 0; i < StrsLen(strs)-1; i++ {
+		result += strs[i] + sep
 	}
 	return result
+}
+
+func StrsLen(strs []string) int {
+	count := 0
+	for range strs {
+		count++
+	}
+
+	return count
 }
