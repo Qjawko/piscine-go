db4024b91c9c53294ed028ea1069deb07dd2fc39
Qjawko <Qjawko@DESKTOP-IDL9KO0>
2019-10-22 16:44:38 +0600

    alphacount.go

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,13 @@ package piscine
 func AlphaCount(str string) int {
 	count := 0
 	for _, ch := range str {
-		for i := 'A'; i <= 'z'; i++ {
+		for i := 'A'; i <= 'Z'; i++ {
+			if ch == i {
+				count++
+			}
+		}
+
+		for i := 'a'; i <= 'z'; i++ {
 			if ch == i {
 				count++
 			}
