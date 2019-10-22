d2ee77559376d53f10b0dc9252ddf4bcb076cf4d
Qjawko <Qjawko@gmail.com>
2019-10-22 20:45:02 +0600

    toupper.go

---
+++ toupper.go
@@ -0,0 +1,22 @@
+package piscine
+
+func ToUpper(s string) string {
+	runes := []rune(s)
+
+	for i := range runes {
+		if IsRuneLowerCase(runes[i]) {
+			runes[i] -= 32
+		}
+	}
+
+	return string(runes)
+}
+
+func IsRuneLowerCase(r rune) bool {
+	for i := 'a'; i < 'z'; i++ {
+		if r == i {
+			return true
+		}
+	}
+	return false
+}
