028bffd3bda75b0e1a18e7162c7b29185d1a4c1b
Qjawko <Qjawko@gmail.com>
2019-10-22 22:58:35 +0600

    isalpha.go

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(str string) bool {
 	for _, r := range str {
-		if !IsRuneDigit(r) || !IsRuneLowerCase(r) || !IsRuneUpperCase(r) {
+		if !(IsRuneDigit(r) || IsRuneLowerCase(r) || IsRuneUpperCase(r)) {
 			return false
 		}
 	}
