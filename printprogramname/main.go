ba7b23b17d192c4c5cf3912632c277ac1652dcef
Qjawko <Qjawko@DESKTOP-IDL9KO0>
2019-10-23 17:41:29 +0600

    fixed

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
 	"os"
 )
 
 func main() {
-	fmt.Println(os.Args[0])
+	PrintRuneArray([]rune(os.Args[0]))
+	_ = z01.PrintRune('\n')
+}
+
+func PrintRuneArray(runes []rune) {
+	for _, ch := range runes {
+		_ = z01.PrintRune(ch)
+	}
 }
