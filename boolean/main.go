8fc4b42e6f35b66aa386a7d7e1e09b30a3ff6c11
Qjawko <Qjawko@gmail.com>
2019-10-28 03:20:37 +0600

    boolean submit:

---
+++ boolean/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(str string) {
+	arrayStr := []rune(str)
+
+	for i := 0; i < len(arrayStr); i++ {
+		z01.PrintRune(arrayStr[i])
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if nbr%2 == 1 {
+		return false
+	}
+	return true
+
+}
+
+func main() {
+	lengthOfArg := getLen(os.Args[1:])
+	EvenMsg := "I have an even number of arguments"
+	OddMsg := "I have an odd number of arguments"
+
+	if isEven(lengthOfArg) {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
+
+func getLen(args []string) int {
+	count := 0
+	for range args {
+		count++
+	}
+
+	return count
+}
